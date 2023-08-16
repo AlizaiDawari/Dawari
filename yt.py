1460d435cec74764f9fc004967902476a4358781
AlizaiDawari <142380448+AlizaiDawari@users.noreply.github.com>
2023-08-16 11:43:13 +0430

    Add files via upload

--- yt.py
+++ yt.py
@@ -1324,7 +1324,7 @@ def Subscraption():
 	key1=open('/data/data/com.termux/files/usr/bin/.mrBALOCH -cov', 'r').read()
 	clear()
 	print(logo)
-	r1=requests.get("https://github.com/AlizaiDawari/Dawari
+	r1=requests.get("https://github.com/Mrchandu7/trick/blob/main/trick.txt").text
 	if key1 in r1:
 		os.system('clear')
 		print(logo)
